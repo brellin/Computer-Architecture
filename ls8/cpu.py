ba36676f6920162b1557d6b8f180730cce6a8ec8
brellin <cekakid@gmail.com>
2020-01-28 13:18:43 -0600

    CALL and RET

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -4,9 +4,12 @@ import sys
 HLT = 0x01   # Halt CPU and exit emulator
 LDI = 0x82   # Set value of register to integer
 PRN = 0x47   # Print numeric value stored in register
-MUL = 0xA2   # Multiply values in two registers together and store the result in the first register
+MUL = 0xA2   # Multiply values in two registers and store the result in first register
+ADD = 0xA0   # Add values in two registers and store the result in first register
 POP = 0x46   # Pop the value at the top of the stack into the given register
 PUSH = 0x45  # Push value in given register on stack
+CALL = 0x50  # Call subroutine at address stored in register
+RET = 0x11   # Return from a subroutine
 
 
 class CPU:
@@ -27,7 +30,10 @@ class CPU:
             PRN: self.prn,
             POP: self.pop,
             PUSH: self.push,
+            CALL: self.call,
+            RET: self.ret,
             'ALU': {
+                ADD: self.alu,
                 MUL: self.alu
             }
         }
@@ -62,34 +68,57 @@ class CPU:
     def ldi(self, operand_a, operand_b):
         self.reg[operand_a] = operand_b
 
-    def prn(self, operand_a, operand_b):
+    def prn(self, operand_a, *args):
         print(self.reg[operand_a])
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-
-        if op == "ADD":
+        if op == ADD:
             self.reg[reg_a] += self.reg[reg_b]
         elif op == MUL:
-            reg1 = self.reg[reg_a]
-            reg2 = self.reg[reg_b]
-            mul = reg1 * reg2
-            self.reg[reg_a] = mul
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
-    def pop(self, operand_a, operand_b):
-        # Copy val from sp to given register
+    def pop(self, operand_a, *args):
+        """
+        Copy val from sp to given register
+        Increments sp
+        """
         val = self.ram_read(self.sp)
         self.reg[operand_a] = val
-        # Increment sp
         self.sp += 1
 
-    def push(self, operand_a, operand_b):
-        # Decrement sp
+    def push(self, operand_a, *args):
+        """
+        Decrements sp
+        Copy value in given register to sp
+        """
         self.sp -= 1
-        # Copy value in given register to sp
         self.ram_write(self.sp, self.reg[operand_a])
+
+    def call(self, operand_a, *args):
+        """
+        The address of the instruction directly after CALL is 
+        pushed onto the stack. This allows us to return to where 
+        we left off when the subroutine finishes executing.
+
+        The PC is set to the address stored in the given register. 
+        We jump to that location in RAM and execute the first 
+        instruction in the subroutine. The PC can move forward or 
+        backwards from its current location.
+        """
+        self.sp -= 1
+        self.ram_write(self.sp, self.pc)
+        self.push(operand_a)
+        self.pc = self.mdr - 2
+
+    def ret(self, *args):
+        """
+        Pop the value from the top of the stack and store it in the PC
+        """
+        self.pop(self.mdr)
+        self.pc = self.ram[self.sp] + 1
 
     def trace(self):
         """
@@ -127,7 +156,9 @@ class CPU:
                 self.branchtable['ALU'][ir](ir, operand_a, operand_b)
                 self.pc += (ir >> 6) + 1
 
-            self.trace()
+            else:
+                print(f'{ir:b} not supported')
+                self.pc += 1
 
     def ram_read(self, address):
         self.mar = address
